db414343ab27e8eebdea6f4295e59fe6f7b78ab3
chrid97 <cgoncal12@gmail.com>
2026-02-24 10:31:42 -0800

    Compute rope length from remaining time instead of using burn accumulator; Make floating hud non focusable

--- lua/rope_timer.lua
+++ lua/rope_timer.lua
@@ -1,58 +1,51 @@
 local M = {}
 local ns = vim.api.nvim_create_namespace("timer")
-local buf = vim.api.nvim_create_buf(false, true)
-local window_width = vim.o.columns
-local duration_ms = 30000
-local tick_ms = 50
 
 local inactivity_timer_ms = 10000
 
-local remaining_ms = duration_ms
-local remaining_rope = window_width
-
-local burn_rate = remaining_rope / duration_ms
-local burn_accumulator = 0.0
+-- local group = vim.api.nvim_create_augroup("RopeTimer", { clear = true })
+--
+-- vim.api.nvim_create_autocmd("InsertCharPre", {
+-- 	group = group,
+-- 	callback = function()
+-- 		vim.schedule()
+-- 	end,
+-- })
 
 function M.setup()
 	M.user_commands()
 end
 
--- function BumpTimer()
--- 	remaining_ms = remaining_ms + 10
--- 	remaining_rope = remaining_rope + 10
--- 	burn_accumulator = 0
--- 	UpdateRopeLength(window_width)
--- end
-
-function M.user_commands()
-	vim.api.nvim_create_user_command("StartTimer", StartTimer, {})
-
-	vim.api.nvim_create_user_command("ReloadRopeTimer", function()
-		require("lazy.core.loader").reload("nvim-rope-timer")
-	end, {})
-end
-
-function StartTimer()
-	-- local group = vim.api.nvim_create_augroup("RopeTimer", { clear = true })
-	--
-	-- vim.api.nvim_create_autocmd("InsertCharPre", {
-	-- 	group = group,
-	-- 	callback = function()
-	-- 		vim.schedule(BumpTimer)
-	-- 	end,
-	-- })
-
-	local rope = string.rep("█", window_width)
-	vim.api.nvim_buf_set_lines(buf, 0, -1, false, { rope })
-	vim.api.nvim_open_win(buf, false, {
+local function open_floating_window()
+	-- vim.api.nvim_set_option_value("modifiable", true, { buf = buf })
+	-- vim.api.nvim_buf_set_lines(buf, 0, -1, false, { "" })
+	-- vim.api.nvim_set_option_value("modifiable", false, { buf = buf })
+	local buf = vim.api.nvim_create_buf(false, true)
+	local win = vim.api.nvim_open_win(buf, false, {
 		relative = "editor",
 		row = 0,
 		col = 0,
-		width = window_width,
+		width = vim.o.columns,
 		height = 1,
+		focusable = false,
 		style = "minimal",
+		noautocmd = true,
 	})
-	print("Starting timer!")
+
+	return buf, win
+end
+
+local function draw_rope(buf, rope_length)
+	local rope = string.rep("█", rope_length)
+	vim.api.nvim_buf_set_lines(buf, 0, -1, false, { rope })
+end
+
+local function start_timer()
+	local duration_ms = 30000
+	local tick_ms = 50
+	local remaining_ms = duration_ms
+
+	local buf, win = open_floating_window()
 
 	local timer = vim.uv.new_timer()
 	if not timer then
@@ -62,35 +55,30 @@ function StartTimer()
 		0,
 		tick_ms,
 		vim.schedule_wrap(function()
-			if remaining_ms <= 0 then
+			if remaining_ms < 0 then
+				if win and vim.api.nvim_win_is_valid(win) then
+					vim.api.nvim_win_close(win, true)
+				end
+				vim.on_key(nil, ns)
 				timer:stop()
 				timer:close()
-				vim.on_key(nil, ns)
-				print("Done!")
 				return
 			end
 
-			burn_accumulator = burn_accumulator + (burn_rate * tick_ms)
-
-			local burn_now = math.floor(burn_accumulator)
-			if burn_now > 0 then
-				burn_accumulator = burn_accumulator - burn_now
-				remaining_rope = remaining_rope - burn_now
-				if remaining_rope < 0 then
-					remaining_rope = 0
-				end
-				print(remaining_rope)
-				UpdateRopeLength(remaining_rope)
-			end
+			local rope_length = math.ceil(vim.o.columns * remaining_ms / duration_ms)
+			draw_rope(buf, rope_length)
 
 			remaining_ms = remaining_ms - tick_ms
 		end)
 	)
 end
 
-function UpdateRopeLength(rope_length)
-	local rope = string.rep("█", rope_length)
-	vim.api.nvim_buf_set_lines(buf, 0, -1, false, { rope })
+function M.user_commands()
+	vim.api.nvim_create_user_command("StartTimer", start_timer, {})
+
+	vim.api.nvim_create_user_command("ReloadRopeTimer", function()
+		require("lazy.core.loader").reload("nvim-rope-timer")
+	end, {})
 end
 
 return M
